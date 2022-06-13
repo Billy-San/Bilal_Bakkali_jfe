bba8bfcd7994603f47e418ba83284d855bf9dc9e
Billy-San <bilal.bakkali@student.ehb.be>
2022-06-13 09:15:40 +0200

    Initial commit

---
+++ src/test/java/com/ehb/examenjava/BilalBakkaliJfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehb.examenjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilalBakkaliJfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
